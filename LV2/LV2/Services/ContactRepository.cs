70b6c17ff1f83bf80ed898a12ce7690db4272aff
Ivan Stakovic <ivan.stakovic@hotmail.com>
2021-11-09 20:49:03 +0100

    Extracting Functionality into a Service Layer

---
+++ LV2/LV2/Services/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using LV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LV2.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+            new Contact
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+            };
+        }
+    }
+}
